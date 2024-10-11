0fe7ae2aea321bd51cc9c32294deb91c29019214
Arnold Melet <arnoldmelet18@gmail.com>
2024-10-11 18:50:09 +0200

    add Account to User schema

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,61 +1,90 @@
-const User = require('../models/User.js')
-const jwt = require('jsonwebtoken')
-const bcryptj = require('bcryptjs')
-
+const User = require("../models/User.js");
+const Account = require("../models/Account.js");
+const jwt = require("jsonwebtoken");
+const bcryptj = require("bcryptjs");
 
 const UserController = {
-    async register(req, res) {
-      try {
-        const passwordHash = await bcryptj.hash(req.body.password, 10);
-        const user = await User.create({
-          ...req.body,
-          role: 'user',
-          password: passwordHash,
-          confirmed: false,
-        });
-        res.status(201).send({ message: 'Usuario registrado con exito', user })
-      } catch (error) {
-        console.error(error)
-        return res.status(400).send({message: 'Error al registrar', error})
-      }
-    },
+  async register(req, res) {
+    try {
+      const passwordHash = await bcryptj.hash(req.body.password, 10);
+      const user = await User.create({
+        ...req.body,
+        role: "user",
+        password: passwordHash,
+        confirmed: false,
+      });
+      res.status(201).send({ message: "Usuario registrado con exito", user });
+    } catch (error) {
+      console.error(error);
+      return res.status(400).send({ message: "Error al registrar", error });
+    }
+  },
 
-   async login(req, res) {
+  async getAll(req, res) {
+    try {
+      const users = await User.find({});
+
+      res.send(users);
+    } catch (error) {
+      console.log(error);
+      res.status(500).send({
+        message: "Ha habido un problema al cargar los usuarios",
+      });
+    }
+  },
+
+  async getAllAccounts(req, res) {
+    try {
+      console.log(req.user);
+      res.send(req.user);
+    } catch (error) {
+      console.log(error);
+      res.status(500).send({
+        message: "Ha habido un problema al cargar los usuarios",
+      });
+    }
+  },
+
+  async login(req, res) {
     try {
       const { dni, password } = req.body;
       const user = await User.findOne({ dni });
       if (!user) {
-          return res.status(400).send('Error: Usuario no encontrado');
+        return res.status(400).send("Error: Usuario no encontrado");
       }
       const isPasswordValid = await bcryptj.compare(password, user.password);
       if (!isPasswordValid) {
-          return res.status(400).send('Error: Contraseña incorrecta');
+        return res.status(400).send("Error: Contraseña incorrecta");
       }
       const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET);
       if (user.tokens.length > 4) user.tokens.shift();
       user.tokens.push(token);
       await user.save();
-      console.log (user)
-      res.send({ message: `Inicio de sesión exitoso ${user.name}` , user, token });
-  } catch (error) {
+      console.log(user);
+      res.send({
+        message: `Inicio de sesión exitoso ${user.name}`,
+        user,
+        token,
+      });
+    } catch (error) {
       console.error(error);
-      res.status(500).send('Error: Ha ocurrido un error al iniciar sesión');
-        }
-    },
+      res.status(500).send("Error: Ha ocurrido un error al iniciar sesión");
+    }
+  },
 
-async logout(req, res) {
+  async logout(req, res) {
     try {
       await User.findByIdAndUpdate(req.user._id, {
         $pull: { tokens: req.headers.authorization },
-      })
-      res.send({ message: 'Desconectado con éxito' })
+      });
+      res.send({ message: "Desconectado con éxito" });
     } catch (error) {
-      console.error(error)
+      console.error(error);
       res.status(500).send({
-        message: 'Hubo un problema al intentar desconectar al usuario',
-      })
+        message: "Hubo un problema al intentar desconectar al usuario",
+      });
     }
   },
-}
+};
 
-  module.exports = UserController
+module.exports = UserController;
--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,13 @@
-const express = require('express')
-const router = express.Router()
-const UserController = require('../controllers/UsersController')
-const { authentication } = require('../middlewares/authentication')
-const User = require('../models/User')
+const express = require("express");
+const router = express.Router();
+const UserController = require("../controllers/UsersController");
+const { authentication } = require("../middlewares/authentication");
+const User = require("../models/User");
 
-router.post('/', UserController.register)
-router.post('/login', UserController.login)
-router.delete ('/logout', authentication,UserController.logout)
+router.post("/", UserController.register);
+router.get("/", authentication, UserController.getAll);
+router.get("/accounts", authentication, UserController.getAllAccounts);
+router.post("/login", UserController.login);
+router.delete("/logout", authentication, UserController.logout);
 
-module.exports = router
+module.exports = router;
