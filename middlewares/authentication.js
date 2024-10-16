c3f5fb90351aa90ccb3a701934c9bdfb7bde1735
Arnold Melet <arnoldmelet18@gmail.com>
2024-10-16 20:39:59 +0200

    prettier correction in authentication

--- middlewares/authentication.js
+++ middlewares/authentication.js
@@ -1,23 +1,24 @@
-const User = require('../models/User')
-const jwt = require('jsonwebtoken')
-require("dotenv").config()
-
+const User = require("../models/User");
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
 
 const authentication = async (req, res, next) => {
-    try {
-      const token = req.headers.authorization
-      const payload = jwt.verify(token, process.env.JWT_SECRET)
-      const user = await User.findOne({ _id: payload._id, tokens: token })
-      if (!user) {
-        return res.status(401).send({ message: 'No estás autorizado' })
-      }
-      req.user = user
-      next()
-    } catch (error) {
-      console.error(error)
-      return res.status(500).send({ error, message: 'Ha habido un problema con el token' })
-    }
-   }
-   
+  try {
+    const token = req.headers.authorization;
 
-module.exports = { authentication }
+    const payload = jwt.verify(token, process.env.JWT_SECRET);
+    const user = await User.findOne({ _id: payload._id, tokens: token });
+    if (!user) {
+      return res.status(401).send({ message: "No estás autorizado" });
+    }
+    req.user = user;
+    next();
+  } catch (error) {
+    console.error(error);
+    return res
+      .status(500)
+      .send({ error, message: "Ha habido un problema con el token" });
+  }
+};
+
+module.exports = { authentication };
