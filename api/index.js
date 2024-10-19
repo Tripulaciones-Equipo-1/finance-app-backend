f94039ac643aa5a4dbebbf4029abbdfbccb9bcac
Zuhaitz Martínez <zuhaitzmartinezjimenez@gmail.com>
2024-10-19 12:31:25 +0200

    vercel config

--- api/index.js
+++ api/index.js
@@ -1,8 +1,8 @@
 require("dotenv").config();
 
 const express = require("express");
-const dbConnection = require("./config/database");
-const { typeError } = require("./middlewares/errors");
+const dbConnection = require("../config/database");
+const { typeError } = require("../middlewares/errors");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -15,7 +15,7 @@ const runServer = async () => {
 
 const cors = require("cors");
 const corsOptions = {
-  origin: "http://localhost:5173",
+  origin: process.env.FRONT_URI || "http://localhost:5173",
   credentials: true, //access-control-allow-credentials:true
   optionSuccessStatus: 200,
 };
@@ -23,11 +23,11 @@ app.use(cors(corsOptions));
 
 app.use(express.json());
 
-app.use("/users", require("./routes/users"));
+app.use("/users", require("../routes/users"));
 
 app.use(typeError);
 
-app.use("/account", require("./routes/account.routes"));
-app.use("/transaction", require("./routes/transaction.routes"));
+app.use("/account", require("../routes/account.routes"));
+app.use("/transaction", require("../routes/transaction.routes"));
 
 runServer();
